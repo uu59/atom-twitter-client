02aa7ca57cf5c8fcb9103ff7dcdb19fb1318fceb
uu59 <k@uu59.org>
2015-03-31 01:53:40 +0900

    Fix markup

--- src/js/components/channels.jsx
+++ src/js/components/channels.jsx
@@ -17,17 +17,17 @@ export default React.createClass({
 
   render() {
     return <div className="channel">
-      <div className="channel--fixed">
+      <section className="channel__fixed">
         <h1>ch</h1>
         <p onClick={this.onClick.bind(this, "home", {})} data-type="home">Home</p>
-      </div>
-      <div className="channel--lists">
+      </section>
+      <section className="channel__lists">
         <h1>lists</h1>
         {this.state.lists.map((list) => {
           var slug = `@${this.props.currentUser}/${list.slug}`;
           return <p key={list.id} onClick={this.onClick.bind(this, "list", {list_id: list.id})} data-type="list" data-slug={{id: list.id}}>{list.name}</p>
         })}
-      </div>
+      </section>
     </div>;
   },
 
